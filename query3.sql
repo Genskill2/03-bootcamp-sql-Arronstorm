59c5d09b42ff633448ff1b827641137939fe38fb
Abhishek Raj Nair <62093284+Arronstorm@users.noreply.github.com>
2021-06-17 10:06:54 +0530

    modified some typo again

--- query3.sql
+++ query3.sql
@@ -2,6 +2,6 @@ select books.title
 from books
 inner join books_subjects
 on books.id = books_subjects.book
-inner join subject
+inner join subjects
 on books_subjects.subject = subjects.id
 where subjects.name IN ('Technology','Politics');
